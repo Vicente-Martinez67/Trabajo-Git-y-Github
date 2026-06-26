3bfe8e9eacd1504ceecb590b631e2990bfa9839b
Vicente Martinez <vic.martinezg@duocuc.cl>
2026-06-25 23:38:39 -0400

    Se agregan funciones de actualizar y completar tareas

--- main.py
+++ main.py
@@ -6,11 +6,14 @@
 tareas=[]
 
 #Creacion funcion con menu:
+#Creacion de dos nuevas opciones que son 3.-: Marcar tareas como completadas y la otra opcion #4.-: Actualizar Tareas
 def menu():
     print("===MENU===")
     print("1.-: Agregar Tarea")
     print("2.-: Listar Tareas")
-    print("3.-: Salir")
+    print("3.-: Marcar tareas como completadas")
+    print("4.-: Actualizar Tareas")
+    print("5.-: Salir")
 
 # Función que solicita ingresar opcion  y valida la opción ingresada:
 def op():
@@ -20,7 +23,7 @@ def op():
 
             if op <1:
                 print("Debe ingresar un numero entero.")
-            elif op <1 or op >3:
+            elif op <1 or op >5:
                 print("Debe escoger una opcion que este entre 1 a 3.")
             else:
                 return op
@@ -37,7 +40,8 @@ def agregarTarea():
         return tareaAgregar
     
     nuevaTarea={
-        "Tarea":tareaAgregar
+        "Tarea":tareaAgregar,
+        "Estado":"Pendiente"
     }
 
     tareas.append(nuevaTarea)
@@ -57,6 +61,47 @@ def listarTareas():
 
 
 
+#Creacion de funcion de completar tarea con su respectiva validacion
+def completarTarea():
+    nombreTarea=input("Ingrese el nombre de la tarea a marcar como completada: ")
+    
+    encontrada = False
+
+    for tarea in tareas:
+        if tarea["Tarea"].lower() == nombreTarea.lower():
+            encontrada = True
+            tarea["Estado"] = "completado"
+            print("Tarea marcada como completada.")
+            return nombreTarea
+    print("Tarea no encontrada")
+
+
+
+
+#Creacion de funcion de actualizar estado de la tarea con su respectiva validacion
+def actualizarEstado():
+    nombreEstado=input("Ingrese el nombre de la tarea: ")
+
+    for tarea in tareas:
+        if tarea["Tarea"].lower() == nombreEstado.lower():
+            nuevo_estado = input("Nuevo estado (Pendiente/Completada): ")
+
+            if nuevo_estado.lower() in ["pendiente", "completada"]:
+                tarea["Estado"]=nuevo_estado
+                print("Estado actualizado")
+            else:
+                print("Estado invalido")
+            return  
+    print("Tarea no encontrada")
+
+
+
+
+
+
+
+
+
 
 
 
@@ -64,12 +109,18 @@ def listarTareas():
 while True:
     menu()
     opcion =op()
-#Agrege las funciones hechas en las opciones.
+#Agregue las funciones hechas en las opciones.
+#Agregue dos nuevas opciones en el main que son la de completarTarea y la de actualizarEstado
     if opcion==1:
         agregarTarea()
     elif opcion==2:
         listarTareas()
     elif opcion ==3:
+        completarTarea()
+    elif opcion ==4:
+        actualizarEstado()
+    elif opcion==5:
         print("¡Gracias por usar el programa!.")
         print("Saliendo........")
-        break
+        break 
+       
